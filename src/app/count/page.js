9241180f46e685351e879ce429e3648c23528de2
Dev_Ak <aakash.velu.003@gmail.com>
2025-09-09 02:36:39 +0530

    completed the logic

--- src/app/count/page.js
+++ src/app/count/page.js
@@ -1,5 +1,7 @@
 "use client"
 import "../../index.css"
+import "../globals.css"
+
 import { useState } from "react"
 
 export default function App() {
@@ -15,17 +17,27 @@ export default function App() {
 
 function Count() {
     const [count, setCount] = useState(0)
+    const [steps, setSteps] = useState(0)
     const data = new Date("2025-09-08");
     data.setDate(data.getDate() + count)
 
     return (
-        <div className="flex flex-col items-center justify-center">
-            <button onClick={() => setCount(c => c + 1)}>+</button>
-            <h1>count:{count} </h1>
-            <button onClick={() => setCount(c => c - 1)}>-</button>
+        <div className="text-center">
 
-            <p>
-                <span>{count === 0 ? " Today is " : count > 0 ? `${count} days from today is ` : `${Math.abs(count)} days ago was `} </span>
+
+            <div className="flex flex-row items-center justify-center gap-5">
+                <button onClick={() => setSteps(c => c - 1)}>-</button>
+                <h1>steps:{steps} </h1>
+                <button onClick={() => setSteps(c => c + 1)}>+</button>
+
+            </div>
+            <div className="flex flex-row items-center justify-center gap-5">
+                <button onClick={() => setCount(c => c - steps)}>-</button>
+                <h1>count:{count} </h1>
+                <button onClick={() => setCount(c => c + steps)}>+</button>
+            </div>
+            <p className="mt-5">
+                <span className="text-2xl font-bold">{count === 0 ? " Today is " : count > 0 ? `${count} days from today is ` : `${Math.abs(count)} days ago was `} </span>
                 <span>{data.toLocaleDateString()}</span>
 
 
