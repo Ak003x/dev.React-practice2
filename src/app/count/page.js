75ce1abfc2242dbee1a3c8b42396b34cc8722db4
Dev_Ak <aakash.velu.003@gmail.com>
2025-09-08 16:21:09 +0530

    started

--- src/app/count/page.js
+++ src/app/count/page.js
@@ -1,7 +1,24 @@
-export default function Count() {
-return(
-    <div>
-        <h1>Count</h1>
-    </div>
-)
+"use client"
+import { useState } from "react"
+
+export default function App() {
+    return (
+        <Count />
+    )
+};
+
+
+
+
+function Count() {
+    const [count, setCount] = useState(1)
+
+    return (
+        <div>
+            <h1>count:{count}</h1>
+
+
+
+        </div>
+    )
 }
