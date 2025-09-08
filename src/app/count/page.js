6479464c3133f3606a45ec1bc7613b705b7cbcf1
Dev_Ak <aakash.velu.003@gmail.com>
2025-09-08 21:04:05 +0530

    refine

--- src/app/count/page.js
+++ src/app/count/page.js
@@ -15,14 +15,22 @@ export default function App() {
 
 function Count() {
     const [count, setCount] = useState(0)
+    const data = new Date("2025-09-08");
+    data.setDate(data.getDate() + count)
 
     return (
-        <div >
+        <div className="flex flex-col items-center justify-center">
             <button onClick={() => setCount(c => c + 1)}>+</button>
-            <h1>count:{count}</h1>
-            <button onClick={() => count > 0 ? setCount(c => c - 1) : ""}>-</button>
+            <h1>count:{count} </h1>
+            <button onClick={() => setCount(c => c - 1)}>-</button>
+
+            <p>
+                <span>{count === 0 ? " Today is " : count > 0 ? `${count} days from today is ` : `${count} days ago was `} </span>
+                <span>{data.toLocaleDateString()}</span>
 
 
+
+            </p>
 
         </div >
     )
